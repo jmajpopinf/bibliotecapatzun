08685bf962da765724e78dc30cec6d58238ffa67
jmajpopinf <jmajpopinf@gmail.com>
2021-04-20 17:49:08 -0600

    proteccion urls

--- apps/libro/urls.py
+++ apps/libro/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path, include
+from django.contrib.auth.decorators import login_required
 from apps.libro.views import index_biblioteca, index_dashboard, LibroRegistrar, LibroListar, LibroUpdate, LibroDelete, LibroLiteratura,LibroMatematica, LibroComunicación, LibroHistoria, LibroVarios
 
 urlpatterns = [
     path('', index_biblioteca, name='index_biblioteca'),
-    path('biblioteca/dashboard', index_dashboard, name='index_dashboard'),
+    path('biblioteca/dashboard', login_required(index_dashboard), name='index_dashboard'),
 
-    path('registrar/', LibroRegistrar.as_view(), name='libro_registrar'),
-    path('listar/', LibroListar.as_view(), name='libro_listar'),
-    path('editar/<pk>/', LibroUpdate.as_view(), name='libro_editar'),
-    path('eliminar/<pk>/', LibroDelete.as_view(), name='libro_eliminar'),
+    path('registrar/', login_required(LibroRegistrar.as_view()), name='libro_registrar'),
+    path('listar/', login_required(LibroListar.as_view()), name='libro_listar'),
+    path('editar/<pk>/', login_required(LibroUpdate.as_view()), name='libro_editar'),
+    path('eliminar/<pk>/', login_required(LibroDelete.as_view()), name='libro_eliminar'),
 
     path('listarL/', LibroLiteratura, name='libro_listar_Literatura'),
     path('listarM/', LibroMatematica, name='libro_listar_Matematica'),
